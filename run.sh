b0c44122b2554b23e580aebb4e5c4ef4ac446856
Niels Højen <niels@hojen.net>
2020-08-13 14:44:30 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ test ${AZURE_MOUNT_POINT}
 rm -rf ${AZURE_MOUNT_POINT}
 mkdir -p ${AZURE_MOUNT_POINT}
 
-blobfuse ${AZURE_MOUNT_POINT} --use-https=true --tmp-path=/tmp/blobfuse/${AZURE_STORAGE_ACCCOUNT} --container-name=${AZURE_STORAGE_ACCOUNT_CONTAINER} -o allow_other
+blobfuse ${AZURE_MOUNT_POINT} --use-https=true --tmp-path=/tmp/blobfuse/${AZURE_STORAGE_ACCOUNT} --container-name=${AZURE_STORAGE_ACCOUNT_CONTAINER} -o allow_other
 
 # run the command passed to us
 exec "$@"
